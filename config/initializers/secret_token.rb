fec1eae3ad0c51fe5b69da1dd6646e41cdf7e831
Matthew Bergman <mzbphoto@gmail.com>
2010-10-29 18:06:22 -0400

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NateCoopersWebSite::Application.config.secret_token = '2a734013777d87392c9dd3accb070639cdbd867c4d565389cf426768104e7d64a1a6884a9f5df1e97e02cf5419b10ee33aa30079c7c8e599c966f87e67f62c0a'
